5b151d4eb3a11754247d3b130ded9d0911004cda
guozeng <331323923@qq.com>
2018-10-26 22:34:53 +0800

    ==

--- webpack-demo/main.js
+++ webpack-demo/main.js
@@ -2,7 +2,7 @@ require('./css/index.css');
 // require('style-loader!css-loader?minimize!./main.css'); 也可以不在配置文件写
 const show = require('./lib/show');
 
-show('hello guozeng!');
+show('hello huyao!');
 $.ajax({
     url: "https://cnodejs.org/",
     type: "get",
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
 
 module.exports = {
     entry: './main.js',
@@ -7,19 +7,32 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist')
     },
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader?minimize']
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        // you can specify a publicPath here
+                        // by default it use publicPath in webpackOptions.output
+                        publicPath: '../'
+                      }
+                    },
+                    "css-loader?minimize"
+                  ]
             }
         ]
-    }
-    /*
+    },
     plugins: [
-        new ExtractTextPlugin({
-            filename: `[name]_[contenthash:8].css`
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         })
     ]
-    */
+    
 }
